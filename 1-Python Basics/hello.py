d3f1a04206956262489599b2dacab1d3bc7a017d
Zeeshan-stack23 <zeeshansingh4@gmail.com>
2025-02-21 20:03:27 +0530

    Delete 1-Python Basics/hello.py

--- 1-Python Basics/hello.py
+++
@@ -1,6 +0,0 @@
-if False:
-    print("Correct Indentation")  # This will execute because the condition is True
-    if True:
-        print("This won't print")  # This will NOT execute because the condition is False
-    print("This will print")       # This will execute because it's outside the inner if block
-    print("Outside the if block")      # This will execute because it's outside all if blocks
